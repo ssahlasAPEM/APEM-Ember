b06fc1480d33536d1f747cc94690d7c0bb949339
apengstrom <andrew@curiousm.com>
2016-07-18 16:11:47 -0400

    Remove ember light table and implement manual tables
    Signed-off-by: apengstrom <andrew@curiousm.com>

--- app/pods/components/opportunities/opp-table/component.js
+++ app/pods/components/opportunities/opp-table/component.js
@@ -1,39 +1,10 @@
 import Ember from 'ember';
-import Table from 'ember-light-table';
-
-const computed = Ember.computed;
 
 export default Ember.Component.extend({
   classNames: ['opp-table'],
   model: null,
-  table: null,
-
-  columns: computed(function() {
-    return [{
-      label: 'NAO #',
-      valuePath: 'id',
-      width: '60px',
-      sortable: true
-    }, {
-      label: 'Sales Person',
-      valuePath: 'apemSalesPerson',
-      width: '150px',
-      sortable: true
-    }, {
-      label: 'Revenue',
-      valuePath: 'potentialAnnualRev',
-      width: '150px',
-      sortable: true
-    }, {
-      label: 'Prod Type',
-      valuePath: 'productType',
-      width: '150px',
-      sortable: true
-    }];
-  }),
 
   init() {
     this._super(...arguments);
-    this.set('table', new Table(this.get('columns'), this.get('model')));
   }
 });
