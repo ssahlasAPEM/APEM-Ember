0c2501de78d49316854da77fa5414feca89d0567
Ralica <ralica@curiousm.com>
2016-07-19 12:15:56 -0400

    .
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/pods/components/users/usr-table/component.js
+++ app/pods/components/users/usr-table/component.js
@@ -6,10 +6,37 @@ export default Ember.Component.extend({
 
   init() {
     this._super(...arguments);
+    //a place to hold all selecter records for this table
+    this.selectedItems = [];
+    //a property controlling the disabled state of the 'Delete User' button
+    this.disableDelete = true;
   },
 
   actions: {
-    deleteUser() {
+    onSelection(record, evt){
+      var selected = evt.currentTarget.checked;
+      record.set('selected', selected);
+      if (selected) {
+        this.get('selectedItems').addObject(record);
+      } else {
+        this.get('selectedItems').removeObject(record);
+      }
+      var numSelected = this.get('selectedItems').length;
+      if(numSelected>0){
+        this.set('disableDelete', false);
+      }else{
+        this.set('disableDelete', true);
+      }
+    },
+    launchConfirmDialog() {
+      this.set('confirmShown', true);
+    },
+    submitConfirm() {
+      // trigger action on parent component
+      this.set('confirmShown', false);
+    },
+    cancelConfirm() {
+      this.set('confirmShown', false);
     }
   }
 });
