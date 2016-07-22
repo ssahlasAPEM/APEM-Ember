75eef3ef4522b2e16bd118f6c761a18321e16966
Ralica <ralica@curiousm.com>
2016-07-22 11:30:11 -0400

    Opportunity edit form
    Signed-off-by: Ralica <ralica@curiousm.com>

---
+++ app/pods/components/opportunities/opt-form/component.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  /** This method helps us pull the model attributes because each-in only works with JSON structured objects*/
+  attributes: Ember.computed(function() {
+    var attrNames = [];
+    var opt = this.model;
+    // Get attributes
+    opt.eachAttribute((name) => attrNames.push(name));
+
+    var attrs = Ember.getProperties(opt, attrNames);
+    return attrs;
+  })
+});
