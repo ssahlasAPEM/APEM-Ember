517f17f06f7f5de17b639b01600a729b993c81f9
apengstrom <andrew@curiousm.com>
2016-08-01 09:22:45 -0400

    Ember user profile injection test
    Signed-off-by: apengstrom <andrew@curiousm.com>

--- app/pods/opportunities/new/route.js
+++ app/pods/opportunities/new/route.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  identity: Ember.inject.service(),
+
   redirect() {
     let newOpportunity = this.store.createRecord('opportunity');
+    //let profile = this.get('identity');
+    let theUser = this.get('identity').get('profile');
+    //console.log( JSON.stringify(identity.profile.username) );
+    //newOpportunity.user = identity;
+    console.log(theUser);
 
     newOpportunity.save().then((savedOpportunity) => {
       this.transitionTo('opportunities.opportunity.detail', savedOpportunity);
