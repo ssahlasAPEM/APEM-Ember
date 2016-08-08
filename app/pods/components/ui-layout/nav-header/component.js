0bb04512bda24e862afb14873dc6a397a8209ab0
Ralica <ralica@curiousm.com>
2016-08-08 09:24:19 -0400

    Show and Hide ‘New NAO’ button based on route transition.
    When in opportunity detail the mockups show no New NAO button. The same applies for when we are creating a new opportunity already.
    
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/pods/components/ui-layout/nav-header/component.js
+++ app/pods/components/ui-layout/nav-header/component.js
@@ -2,5 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['nav-header'],
-  identity: Ember.inject.service()
+  identity: Ember.inject.service(),
+  router: Ember.inject.service("-routing"),
+
+  //observe changes to the router
+  didInsertElement: function() {
+    let r = this.get("router");
+    r.addObserver("currentRouteName", this, "onRouteChange");
+  },
+
+  //when the router is in opportunity.detail - hide the new NAO button
+  "onRouteChange": function(router) {
+    let newNAObtn = Ember.$('.new-nao-btn');
+    // console.log("Current route", router.get("currentRouteName"));
+    if(router.get("currentRouteName") === 'opportunities.opportunity.detail'){
+      newNAObtn.addClass('hidden');
+    }else{
+      newNAObtn.removeClass('hidden');
+    }
+  }
 });
