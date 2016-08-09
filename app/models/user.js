c84e4e27220dc9565a9bec7ba85694e4369ce32a
Ralica <ralica@curiousm.com>
2016-08-09 15:06:47 -0400

    User Validations -inactive
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/models/user.js
+++ app/models/user.js
@@ -1,29 +1,42 @@
 import DS from 'ember-data';
-import {
-  validator, buildValidations
-}
-from 'ember-cp-validations';
-const Validations = buildValidations({
-  username: [
-    validator('presence', true),
-    validator('length', {
-      min: 4
-    })
-  ],
-  password: [
-    validator('presence', true),
-    validator('length', {
-      min: 4
-    })
-  ],
-  passwordVerify:[
-    validator('presence', true),
-    validator('confirmation', {
-      on: 'password',
-      message: '{description} do not match',
-      description: 'Passwords'
-    })
-  ]
+import { validator, buildValidations } from 'ember-cp-validations';
+
+var Validations = buildValidations({
+  username: {
+    description: 'Username',
+    validators: [
+      validator('presence', true),
+      validator('length', {
+        min: 5,
+        max: 15
+      })
+    ]
+  },
+  password: {
+    description: 'Password',
+    validators: [
+      validator('presence', true),
+      validator('length', {
+        min: 4,
+        max: 10
+      }),
+      validator('format', {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/,
+        message: '{description} must include at least one upper case letter, one lower case letter, and a number'
+      }),
+      validator('length', {
+        isWarning: true,
+        min: 6,
+        message: 'Your password is not secure.'
+      })
+    ]
+  },
+  passwordConfirmation: validator('confirmation', {
+    on: 'password',
+    message: 'Passwords do not match'
+  })
+}, {
+  debounce: 500
 });
 
 export default DS.Model.extend( Validations, {
