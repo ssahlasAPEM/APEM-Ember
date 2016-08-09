6ad5b416520b2f198e04f2063a622bff00aa9b59
Ralica <ralica@curiousm.com>
2016-08-09 15:11:20 -0400

    .
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/pods/components/new-user/component.js
+++ app/pods/components/new-user/component.js
@@ -1,42 +1,12 @@
 import Ember from 'ember';
 import { validator, buildValidations } from 'ember-cp-validations';
 
-const {
-  isEmpty,
-  computed,
-  defineProperty,
-} = Ember;
-
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   users:Ember.inject.service(),
   userTypes: ["Admin","User"],
   model:null,
 
-  //validation relevant attributes
-  classNames: ['validated-input'],
-  classNameBindings: ['showErrorClass:has-error', 'isValid:has-success'],
-
-  init(){
-    this._super(...arguments);
-    var valuePath = this.get('valuePath');
-    defineProperty(this, 'validation', computed.oneWay(`model.validations.attrs.${valuePath}`));
-    defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
-  },
-
-  notValidating: computed.not('validation.isValidating'),
-  didValidate: computed.oneWay('targetObject.didValidate'),
-  hasContent: computed.notEmpty('value'),
-  isValid: computed.and('hasContent', 'validation.isValid', 'notValidating'),
-  isInvalid: computed.oneWay('validation.isInvalid'),
-  showErrorClass: computed.and('notValidating', 'showMessage', 'hasContent', 'validation'),
-  showErrorMessage: computed('validation.isDirty', 'isInvalid', 'didValidate', function() {
-    return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isInvalid');
-  }),
-
-  showWarningMessage: computed('validation.isDirty', 'validation.warnings.[]', 'isValid', 'didValidate', function() {
-    return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isValid') && !isEmpty(this.get('validation.warnings'));
-  }),
 
   actions:{
 
