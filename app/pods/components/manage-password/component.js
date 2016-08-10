586f5979e4ec82a29765c5c4ba888301d7b18bc4
Ralica <ralica@curiousm.com>
2016-08-10 15:22:37 -0400

    Manage password form validation
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/pods/components/manage-password/component.js
+++ app/pods/components/manage-password/component.js
@@ -5,13 +5,41 @@ export default Ember.Component.extend({
   identity:Ember.inject.service(),
   sessionUser:null,
 
+  didInsertElement(){
+    this._super(...arguments);
+    //add validation to form
+    Ember.$('.managePasswords')
+      .form({
+        inline : true,
+        fields: {
+          password   : {
+            identifier:'password',
+            rules: [
+              {
+                type   : 'empty'
+              }
+            ]
+          },
+          passwordVerify : {
+            identifier  : 'passwordVerify',
+            rules: [
+              {
+                type   : 'match[password]',
+                prompt : 'Passwords do not match.'
+              }
+            ]
+          }
+        }
+    });
+  },
+
   actions:{
 
     openModal:function(){
       this.set('serverErrors',[]);
-      this.set('sessionUser', null);
 
       let thisUserId = this.get('identity').get('profile').get('id');
+
       this.get('store').findRecord('user', thisUserId).then((data) => {
           this.set('sessionUser',data);
         }, (error) => {
@@ -28,26 +56,34 @@ export default Ember.Component.extend({
 
 
     editPassword() {
-      let user = this.get('sessionUser');
-      this.set('serverErrors',[]);
-      let errs = this.get('serverErrors');
-      if (user.get('hasDirtyAttributes')) {
-        let closeModal = this.closeModal;
-        user.save().then(() => {
-          closeModal();
-        }, (error) => {
-          errs.addObject(error);
-        });
+      let hasErrors = Ember.$('.error');
+
+      if(hasErrors.length === 0){
+        let user = this.get('sessionUser');
+        this.set('serverErrors',[]);
+        let errs = this.get('serverErrors');
+        if (user.get('hasDirtyAttributes')) {
+          let closeModal = this.closeModal;
+          user.save().then(() => {
+            closeModal();
+          }, (error) => {
+            errs.addObject(error);
+          });
+        }
       }
     },
 
     cancelModal:function(){
       this.closeModal();
     }
-
   },
 
   closeModal:function(){
+
+    // this.set('sessionUser', null);
+    //clear the form
+    Ember.$('.managePasswords').form('clear');
+
     Ember.$('.manage-password-pop')
     .modal('hide');
   }
