a14606d8baaf5cb30f85030b022806ad1bb104b2
Ralica <ralica@curiousm.com>
2016-08-11 16:50:27 -0400

    Form Management functionality
    check uncheck visible and required relationship.
    
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/pods/components/fields/fld-table/component.js
+++ app/pods/components/fields/fld-table/component.js
@@ -4,16 +4,25 @@ export default Ember.Component.extend({
   classNames: ['fld-table'],
   model: null,
 
-  init() {
-    this._super(...arguments);
-  },
-
   actions:{
-    onVisibleChange:function(){
-
+    onVisibleChange:function(record){
+      //  if visible is unchecked, required is unchecked and disabled
+      if(record.get('visible')===true){
+        record.set('visible',false);
+        record.set('required',false);
+      }else {
+        record.set('visible',true);
+      }
+      record.save();
     },
-    onRequiredChange:function(){
 
+    onRequiredChange:function(record){
+      if(record.get('required')===true){
+        record.set('required',false);
+      }else {
+        record.set('required',true);
+      }
+      record.save();
     }
   }
 });
