2a7c50b66460bc39ceee0ac8e7053c396f7c2e7b
Ralica <ralica@curiousm.com>
2016-08-22 15:12:25 -0400

    New NAO form ront-end validation
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/pods/components/opportunities/opt-form/component.js
+++ app/pods/components/opportunities/opt-form/component.js
@@ -19,17 +19,64 @@ export default Ember.Component.extend({
     {'label':'production', 'id':4},
   ],
 
+  didRender(){
+    this._super(...arguments);
+    let myFields = this.get('fields'),
+    requiredFormValidations = {},
+    fieldsArray = myFields.get('content'),
+    fLen = 0;
+
+    try {
+      fLen = fieldsArray.get('length');
+    } catch (e) { }
+
+    if( fLen > 0 ){
+      fieldsArray.forEach(function(item/*, index, enumerable*/){
+        if (item.get('required') === true) {
+          let fieldName = item.get('name');
+          requiredFormValidations[fieldName] = 'empty';
+            //r: for more detailed validation this structure should be used:
+            // {
+            //   identifier:fieldName,
+            //   rules:[
+            //     {
+            //       type:'empty',
+            //       prompt:'Please enter a ' + fieldName
+            //     }
+            //   ]
+            // }
+          // );
+        }
+      });
+
+      //add validation to form
+      Ember.$('.opportunity-form')
+        .form({
+          inline : false,
+          on:'blur',
+          fields: requiredFormValidations
+      });
+
+      //ensure prevent default behavior...because semantic and ember work well together on occasion..
+      Ember.$('.opportunity-form').submit(function(e){
+          //e.preventDefault(); usually use this, but below works best here.
+          return false;
+      });
+
+    }
+  },
+
   /** This method helps us pull the model attributes because each-in only works with JSON structured objects*/
-  attributes: Ember.computed(function() {
-    let attrNames = [];
-    let opt = this.get('model');
-
-    // Get attributes
-    opt.eachAttribute((name) => attrNames.push(name));
-
-    let attrs = Ember.getProperties(opt, attrNames);
-    return attrs;
-  }),
+  // attributes: Ember.computed(function() {
+  //   let attrNames = [];
+  //   let opt = this.get('model');
+  //
+  //   // Get attributes
+  //   opt.eachAttribute((name) => attrNames.push(name));
+  //
+  //   let attrs = Ember.getProperties(opt, attrNames);
+  //   return attrs;
+  // }),
 
   actions: {
     onDropdownSelect(fieldName, selectedValue){
@@ -47,25 +94,28 @@ export default Ember.Component.extend({
       console.log('cancel opt method reached!');
     },
 
-    updateOpportunity:function() {
-      // Update the opportunity
-      let opt = this.get('model');
-      this.set('serverErrors',[]);
-      let errs = this.get('serverErrors'),
-      sessionUser = this.get('identity').get('profile');
-      // let myRouting = this.get('routing');
-      // debugger;
-      if (opt.get('hasDirtyAttributes')) {
+    updateRecord:function() {
+      //Ember.$('.opportunity-form').form('validate');
+      let hasErrors = Ember.$('.field.error');
+      if(hasErrors.length === 0){
+        // Update the opportunity
+        let opt = this.get('model');
+        this.set('serverErrors',[]);
+        let errs = this.get('serverErrors'),
+        sessionUser = this.get('identity').get('profile');
+        // let myRouting = this.get('routing');
+        if (opt.get('hasDirtyAttributes')) {
+          opt.set('user', sessionUser);
 
-        opt.set('user', sessionUser);
+          console.log('Updating Opportunity...');
 
-        console.log('Updating Opportunity...');
-
-        opt.save().then(() => {
-          this.sendAction('onOptSave');
-        }, (error) => {
-          errs.addObject(error);
-        });
+          opt.save().then(() => {
+            this.sendAction('onOptSave');
+            console.log('Opportunity Saved');
+          }, (error) => {
+            errs.addObject(error);
+          });
+        }
       }
     }
   }
