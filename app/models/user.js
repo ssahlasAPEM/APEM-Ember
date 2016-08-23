a1a04e776e2ed58ac77feca08cf2ef805cb9e7da
Ralica <ralica@curiousm.com>
2016-08-23 10:57:19 -0400

    code cleanup
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/models/user.js
+++ app/models/user.js
@@ -12,5 +12,5 @@ export default DS.Model.extend({
   active: DS.attr('string', { defaultValue: true }),
   numOpportunities: DS.attr('string', { defaultValue: 0 }),
   lastLogin: DS.attr('string', { defaultValue: null }),
-  selected:DS.attr('boolean', { defaultValue: false }),
+  selected:DS.attr('boolean', { defaultValue: false })
 });
