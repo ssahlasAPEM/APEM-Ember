23315cbe881f80ecb0d95d5bd51f563bd039f0a8
Ralica <ralica@curiousm.com>
2016-08-25 10:06:39 -0400

    Adjustment to if-odd helper
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/helpers/if-odd.js
+++ app/helpers/if-odd.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper( function(conditional, options) {
-  if((conditional % 2) == 0) {
+  if((conditional % 2) === 0) {
     return options.fn(this);
   } else {
     return options.inverse(this);
