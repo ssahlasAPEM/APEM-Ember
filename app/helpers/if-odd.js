b780a7b58ca1cc9aeb7cccf2bc6e4e6eec774633
Ralica <ralica@curiousm.com>
2016-08-25 10:03:23 -0400

    Odd number check helper
    Signed-off-by: Ralica <ralica@curiousm.com>

---
+++ app/helpers/if-odd.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Helper.helper( function(conditional, options) {
+  if((conditional % 2) == 0) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
---
+++ tests/unit/helpers/if-odd-test.js
@@ -0,0 +1,10 @@
+import { ifOdd } from 'apem/helpers/if-odd';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | if odd');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = ifOdd([42]);
+  assert.ok(result);
+});
