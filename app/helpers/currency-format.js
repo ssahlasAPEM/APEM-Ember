eb9eaa2cb2bbcc0e67c4a941e1d2005d278bc1f8
Ralica <ralica@curiousm.com>
2016-09-01 10:01:27 -0400

    remove decimals from currency form. removed dormat-currency addon. do npm prune;
    Signed-off-by: Ralica <ralica@curiousm.com>

--- app/helpers/currency-format.js
+++ app/helpers/currency-format.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 
 export function currencyFormat(params/*, hash*/) {
-  var formatted = parseFloat(params, 10).toFixed(2);
+  // var formatted = parseFloat(params, 10).toFixed(2);
+  //
+  // return '$' + formatted;
+  var n = params[0],
+      c = isNaN(c = Math.abs(params[1])) ? 2 : c,
+    //  d = '.',
+      t = ',',
+      i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + '',
+      j = (j = i.length) > 3 ? j % 3 : 0;
 
-  return '$' + formatted;
+      //uncomment end of return for decimals
+  return '$' + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + t);// + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : '');
 }
 
 export default Ember.Helper.helper(currencyFormat);
